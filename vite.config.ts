133008fe9186ef87c4731c4e34526502904bfa24
dev_wandry <dima.development@wandry.com.ua>
2026-01-12 00:00:38 +0200

    fix(build): disable wasm from nitro

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,21 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
-// import { postgres } from "vite-plugin-db";
+import { postgres } from "vite-plugin-db";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
 	plugins: [
 		devtools(),
-		nitro(),
-		// postgres({}),
+
+		postgres({
+			seed: {
+				type: "sql-script",
+				path: "db/init.sql",
+			},
+			referrer: "create-tanstack",
+			dotEnvKey: "VITE_DATABASE_URL",
+		}),
 		// this is the plugin that enables path aliases
 		viteTsConfigPaths({
 			projects: ["./tsconfig.json"],
@@ -19,6 +26,9 @@ const config = defineConfig({
 		tailwindcss(),
 		tanstackStart(),
 		viteReact(),
+		nitro({
+			wasm: false,
+		}),
 	],
 });
 
